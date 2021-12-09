92f0423af6c00695bbcbbd400cc56802471c76cc
三峽高鹽質 <57086376+chihuahua92@users.noreply.github.com>
2021-12-09 11:27:11 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -21,10 +21,7 @@ function redirect(){
   </div>
   <div id='footer'> <a href='#'>homepage</a> | <a href='mailto:denise@mitchinson.net'>contact</a> | <a href='http://validator.w3.org/check?uri=referer'>html</a> | <a href='http://jigsaw.w3.org/css-validator'>css</a> | &copy; 2007 Anyone | Design by <a href='http://www.mitchinson.net'> www.mitchinson.net</a><br/>
     This work is licensed under a <a rel='license' href='http://creativecommons.org/licenses/by/3.0/'>Creative Commons Attribution 3.0 License</a> </div>
-</div>
-</body>
-</html>
-";
+</div> ";
     }else if(targetPath == "linebot"){
         content.innerHTML = linebot;
    
