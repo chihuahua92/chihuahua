07d7114dfe6e7a57303c9b66bc8631c19ec85b29
三峽高鹽質 <57086376+chihuahua92@users.noreply.github.com>
2021-12-09 11:58:09 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ function redirect(){
     let rootRe = new RegExp(root);
     let targetPath = window.location.href.replace(rootRe, '').replace("/#!", '').replace('/', '');
     if (targetPath == ""){
-      content.innerHTML = "<h2>Welcome to <span style='font-weight:bold; color:#B07E13;'>Autumn Daze</span> Template</h2><blockquote> This template has been tested in Mozilla Firefox and IE7. The page validates as XHTML 1.0 Transitional using valid CSS. It will work in browser widths of 800x600, 1024x768 &amp; 1280x1064.<br/>For more FREE CSS templates visit <a href='http://www.mitchinson.net'>my website</a>.</blockquote>";
+      content.innerHTML = "<h2>Welcome to Autumn Daze Template</h2><blockquote> This template has been tested in Mozilla Firefox and IE7. The page validates as XHTML 1.0 Transitional using valid CSS. It will work in browser widths of 800x600, 1024x768 &amp; 1280x1064.</blockquote>";
     }else if(targetPath == "linebot"){
         content.innerHTML = linebot;
    
