29b2ac2b2815ada162705665b2575ddb42037097
chihuahua92 <57086376+chihuahua92@users.noreply.github.com>
2022-01-25 11:05:25 +0800

    Create popzaki.js

---
+++ pages/popzaki.js
@@ -0,0 +1,6 @@
+export const  = `
+<h2>PopZaki<h2>
+ <p>仿造紅極一時的迷因遊戲Popcat製作出的點擊計數器，點擊紀錄將儲存在localsotage，點擊時有波波波音效，照片為作者家中養的黑貓。</p>
+ <a href="https://codepen.io/CHIHWA/full/JjrygqW"> <button>Click Me</button> </a>
+  
+;`
