c1d960e0809237699a8f891e3d4f677f17acee4e
chihuahua92 <57086376+chihuahua92@users.noreply.github.com>
2022-01-25 11:09:31 +0800

    Update linebot.js

--- pages/linebot.js
+++ pages/linebot.js
@@ -2,4 +2,4 @@ export const linebot = `
 <h2>linebot<h2>
   <p>用Google App Script加上Node.js開發的爬蟲行LINE機器人，掃描QR CODE加入Line好友即可開始使用服務，此爬蟲機器人可爬台銀壞率資料跟UDN即時新聞</p>
   <img alt="" src="https://upload.cc/i1/2021/12/09/c0AOyd.png"/>
-;`
+`
